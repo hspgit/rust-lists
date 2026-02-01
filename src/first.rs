e3eaca4672e356a2c0868962c0db485fdb8e08cc
Hrishikesh Pradhan <hspsat@gmail.com>
2026-01-31 23:30:13 -0500

    2.4 - Push

--- src/first.rs
+++ src/first.rs
@@ -25,6 +25,8 @@
 //     More(Box<Node>),
 // }
 
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -38,4 +40,21 @@ struct Node {
     elem: i32,
     next: Link,
 }
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+    let new_node = Box::new(Node {
+        elem: elem,
+        next: mem::replace(&mut self.head, Link::Empty),
+    });
+
+    self.head = Link::More(new_node);
+}
+
+}
+
 
