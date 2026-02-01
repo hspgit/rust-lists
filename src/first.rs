f8eda0badfaf97a503e77d371df2787bc1f552e3
Hrishikesh Pradhan <hspsat@gmail.com>
2026-02-01 15:02:56 -0500

    2.5 Pop

--- src/first.rs
+++ src/first.rs
@@ -1,13 +1,12 @@
 // pub enum List {
 //     Empty,
 //     Elem(i32, List)
-// } 
+// }
 
 // pub enum List {
 //     Empty,
 //     Elem(i32, Box<List>),
 // }
-
 
 // pub enum List {
 //     Empty,
@@ -47,14 +46,21 @@ impl List {
     }
 
     pub fn push(&mut self, elem: i32) {
-    let new_node = Box::new(Node {
-        elem: elem,
-        next: mem::replace(&mut self.head, Link::Empty),
-    });
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
 
-    self.head = Link::More(new_node);
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
 }
-
-}
-
-
